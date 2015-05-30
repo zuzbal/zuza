b486f47a4d007e1955259909632ae59944fc3209
Zuza <zuzbal@gmail.com>
2015-05-30 13:19:26 +0000

    i can add

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -2,5 +2,22 @@ class DestinationsController < ApplicationController
   def index
     @destinations = Destination.all
     
+    
+  end
+  
+  def new
+    @destination = Destination.new 
+  end
+  
+
+    def create
+      Destination.create!(destination_params)
+      redirect_to destinations_path
+    end
+    
+    def destination_params
+      params.require(:destination).permit(:name, :transport_type, :description, :start_at,   :end_at, :photo_url)
+      
+
   end
 end
